e1d4f7f5cff9c854b84d9b7a90251924c6037d97
Dmitrii (Scern) Didenko <scern86@gmail.com>
2024-02-26 09:35:25 +0200

    Update Pdo.php

--- Application/Pdo.php
+++ Application/Pdo.php
@@ -3,18 +3,29 @@
 namespace Lira\Application;
 
 use Lira\Framework\Database\DatabaseInterface;
+use Lira\Framework\Events\Dispatcher;
+use Lira\Framework\Events\Event;
+use Lira\Framework\Events\EventsSupport;
+use Lira\Framework\Events\Level;
+use Lira\Framework\Events\Type;
 
 class Pdo implements DatabaseInterface
 {
-    protected \PDO $pdo;
+    use EventsSupport;
+    protected ?\PDO $pdo = null;
 
-    public function __construct(string $database, string $user, string $password, string $host = '127.0.0.1', int $port = 5432)
+    public function __construct(Dispatcher $eventDispatcher,string $database, string $user, string $password, string $host = '127.0.0.1', int $port = 5432)
     {
-        $dsn = "pgsql:host={$host};port={$port};dbname={$database}";
-        $this->pdo = new \PDO($dsn, $user, $password);
+        $this->initEventDispatcher($eventDispatcher);
+        try {
+            $dsn = "pgsql:host={$host};port={$port};dbname={$database}";
+            $this->pdo = new \PDO($dsn, $user, $password);
+        }catch (\Throwable $e){
+            $this->eventDispatcher->dispatch(new Event(Type::ERROR,Level::WARNING,'Can`t create database connection',[$e]));
+        }
     }
 
-    public function connect(): \PDO
+    public function connect(): ?\PDO
     {
         return $this->pdo;
     }
