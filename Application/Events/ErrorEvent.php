7bee2c781ade98027cdf203748042791db81c63d
Dmitrii (Scern) Didenko <scern86@gmail.com>
2024-02-27 09:53:59 +0200

    Add some classes of Events

---
+++ Application/Events/ErrorEvent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Lira\Application\Events;
+
+readonly class ErrorEvent extends \Lira\Framework\Events\Event
+{
+
+}
---
+++ Application/Events/SecurityEvent.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Lira\Application\Events;
+
+readonly class SecurityEvent extends \Lira\Framework\Events\Event
+{
+
+}
