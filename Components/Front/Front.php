3ca728b8f9b534ede668cd62b1f14a41a86d6e92
Dmitrii (Scern) Didenko <scern86@gmail.com>
2024-03-04 09:48:20 +0200

    Add Front component

---
+++ Components/Front/Front.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Lira\Components\Front;
+
+use Lira\Application\App;
+use Lira\Framework\Results\Result;
+use Lira\Application\Result\{Success,Error,Json,Redirect,InternalRedirect};
+use Lira\Components\DefaultController;
+use Lira\Framework\{Controller,Router};
+use Symfony\Component\HttpFoundation\Response;
+
+class Front extends Controller
+{
+    const COMPONENT_DIR = ROOT_DIR . DS . 'Components' . DS . 'Front';
+
+    public function __construct()
+    {
+        $app = App::getInstance();
+        //$app->view->addHeaderLink('<link rel="stylesheet" href="/assets/css/style.min.css">');
+        //$app->view->addBodyLink('<script defer src="/assets/js/script.min.js"></script>');
+
+        $app->database->init();
+    }
+
+    public function execute(string $uri): Result
+    {
+        try {
+            $app = App::getInstance();
+
+            $view = $app->view;
+            $view->template = self::COMPONENT_DIR.DS.'templates'.DS.'default.inc';
+
+            $router = new Router(DefaultController::class,$app->config->get('routes')->front);
+            $class = $router->execute($uri);
+            $controller = new $class();
+
+            $result = $controller->execute($uri);
+
+            switch ($result::class) {
+                case Success::class:
+                    $view->content = $result->content;
+                    return new Success($view->render(), $result->statusCode, $result->headers);
+                case Error::class:
+                    $view->content = $result->content;
+                    return new Error($view->render(), $result->statusCode, $result->headers);
+                case Json::class:
+                case Redirect::class:
+                case InternalRedirect::class:
+                    return $result;
+                default:
+                    return new Error('Controller error');
+            }
+        } catch (\Throwable $e) {
+            return new Error($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
