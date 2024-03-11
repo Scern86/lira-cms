4e5fdc57d1127e8ff6a9086f27865c30806619ee
Dmitrii (Scern) Didenko <scern86@gmail.com>
2024-03-11 10:15:34 +0200

    Update Index.php

--- Components/Admin/Index/Index.php
+++ Components/Admin/Index/Index.php
@@ -5,7 +5,7 @@ namespace Lira\Components\Admin\Index;
 use Lira\Application\App;
 use Lira\Components\Admin\Admin;
 use Lira\Framework\Results\Result;
-use Lira\Application\Result\{Success,Error};
+use Lira\Application\Result\{Redirect, Success, Error};
 use Lira\Framework\View;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,6 +15,8 @@ class Index extends \Lira\Framework\Controller
 
     public function execute(string $uri): Result
     {
+        if(!App::getInstance()->user->isLoggedIn()) return new Redirect(Admin::COMPONENT_URL.'/login');
+
         $view = new View(self::CONTROLLER_DIR.DS.'template.inc');
 
         App::getInstance()->view->meta_title = 'Admin panel | LiraCMS';
