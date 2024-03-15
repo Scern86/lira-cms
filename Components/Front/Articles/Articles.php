4f4e6db24e3f896a2a69b6d9c8bec89cc041ab06
Dmitrii (Scern) Didenko <scern86@gmail.com>
2024-03-15 09:44:59 +0200

    Add Front Articles controller

---
+++ Components/Front/Articles/Articles.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Lira\Components\Front\Articles;
+
+use Lira\Application\App;
+use Lira\Application\Models\Category;
+use Lira\Framework\Results\Result;
+use Lira\Application\Result\{Success,Error};
+use Lira\Components\Front\Front;
+use Lira\Framework\View;
+use Symfony\Component\HttpFoundation\Response;
+
+class Article extends \Lira\Framework\Controller
+{
+    protected \Lira\Application\Models\Article $model;
+    const CONTROLLER_DIR = Front::COMPONENT_DIR . DS . 'Articles';
+
+    public function __construct()
+    {
+        $this->model = new \Lira\Application\Models\Article(App::getInstance()->database);
+    }
+
+    public function execute(string $uri): Result
+    {
+        if($uri!=='/articles') return $this->showOne($uri);
+        return $this->list();
+    }
+
+    private function list(): Result
+    {
+        $canonicalUrl = '/articles';
+
+        $categoryModel = new Category(App::getInstance()->database);
+        $articles = $categoryModel->getArticlesByCategoryUrl($canonicalUrl);
+
+        $view = new \Lira\Application\View(App::getInstance()->lexicon,self::CONTROLLER_DIR.DS.'template.inc');
+        $view->articles = $articles;
+
+        App::getInstance()->view->meta_title = 'Articles';
+
+        return new Success($view->render(),Response::HTTP_OK);
+    }
+
+    private function showOne(string $url): Result
+    {
+        $canonicalUrl = $url;
+
+        $article = $this->model->getArticleByUrl($canonicalUrl);
+        if(empty($article)) return new Error('404 Page not found',Response::HTTP_NOT_FOUND);
+
+        $view = new View(self::CONTROLLER_DIR.DS.'show.inc');
+        $view->article = $article;
+
+        App::getInstance()->view->meta_title = $article['title'];
+
+        return new Success($view->render(),Response::HTTP_OK);
+    }
+}
